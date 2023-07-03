5b08e987a56e6376fc344c198b5c78d191fefd2c
Joseph21 <93709765+Joseph21-6147@users.noreply.github.com>
2023-07-03 22:19:34 +0200

    Add files via upload

---
+++ part 24a (isolated RC_Misc)/RC_Misc.h
@@ -0,0 +1,35 @@
+#ifndef RC_MISC_H
+#define RC_MISC_H
+
+#define PI 3.1415926535f
+
+// this constant controls the significance of the trig lookup functions
+#define SIG_POW10 100      // float angle is rounded at two decimal points (use 1000 for three, etc)
+
+// this constant controls the significance of the float_rand_between() function
+#define F_SIGNIF  1000.0f
+
+// ==============================/  Prototypes for convenience and look up trig functions  /==============================
+
+// convenience conversion functions
+float deg2rad( float fAngleDeg );
+float rad2deg( float fAngleRad );
+
+// convenience modulo functions
+float mod360( float fAngleDeg );
+float mod2pi( float fAngleRad );
+
+// look up sine and cosine functions: init and call
+void init_lu_sin_array();
+void init_lu_cos_array();
+
+float lu_sin( float fDegreeAngle );
+float lu_cos( float fDegreeAngle );
+
+// convenience functions for random range integers and floats
+int     int_rand_between( int   nLow, int   nHgh );    // returns a random integer in the range [ nLow, nHgh ]
+float float_rand_between( float fLow, float fHgh );    // returns a random float   in the range [ fLow, fHgh ]
+
+// ==============================/  end of file   /==============================
+
+#endif // RC_MISC_H
